e3f03d3603af1da830ce3fd99ec4b60cc7c828e7
Gary <Gary.Dix@enform.nz>
2022-08-30 14:31:33 +1200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VisitorManagement.Models;
+
+namespace VisitorManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<VisitorManagement.Models.StaffNames>? StaffNames { get; set; }
+        public DbSet<VisitorManagement.Models.Visitor>? Visitor { get; set; }
+    }
+}
