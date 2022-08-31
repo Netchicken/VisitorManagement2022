480f48aaa268b7737d6b98edd01608d10f98f1ad
Gary <Gary.Dix@enform.nz>
2022-08-31 14:30:08 +1200

    Adding in the text file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,13 +10,36 @@ namespace VisitorManagement.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Welcome = "Welcome to the VMS";
+
+            ViewBag.VisitorNew = new Visitor()
+            {
+                FirstName = "Howard",
+                LastName = "The Barbarian"
+            };
+
+            ViewData["AnotherWelcome"] = "Please enter your Name";
+
+            //======= Conditions of Acceptance
+            //Gets or sets the absolute path to the directory that contains the web-servable application content files.
+            string rootPath = _webHostEnvironment.WebRootPath;
+            //Get a path to the file by adding the filename to the path
+            FileInfo filePath = new FileInfo(Path.Combine(rootPath, "ConditionsForAdmittance.txt"));
+            //now we have access to the file, lets read all the lines in to a string array, each line is a seperate entry in the array
+            string[] lines = System.IO.File.ReadAllLines(filePath.ToString());
+            //then pass the string array of the conditions to the ViewData to show on the front.
+            ViewData["Conditions"] = lines;
+
             return View();
         }
 
